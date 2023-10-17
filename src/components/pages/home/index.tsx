2b83564dc336f8f27e03f6de97d28efa2d75737f
Baljinnyam Dayan <Baljinnyam.dayan@gmail.com>
2023-10-17 17:10:03 +0800

    cloudisgn

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -11,42 +11,57 @@ export type HeroHeader = {
 
 export const heroHeader: HeroHeader = {
   header: `Cloudsign`,
-  subheader: `Анхны үүлэн тоон гарын үсгийн систем`,
-  image: `/hero-img.webp`,
+  subheader: `Монголын Анхны үүлэн тоон гарын үсгийн систем`,
+  image: `/cloud.png`,
 };
 
 export default function HeroHeader() {
   return (
-    <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
+    <section className="col container flex flex-col gap-4 pb-12 pt-4 text-center lg:flex-row lg:items-center lg:gap-8 ">
       <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
-        <div className="space-y-4">
-          <h1 className="text-3xl font-black lg:text-6xl">
-            {heroHeader.header}
-          </h1>
-          <h2 className="text-lg font-light text-muted-foreground lg:text-3xl">
+        <div className="space-y-2">
+          <div className="relative">
+            <h1 className="text-start text-6xl font-black text-cyan-950 lg:text-6xl">
+              {heroHeader.header}
+            </h1>
+            <div className="absolute bottom-0 left-4 z-[-11] h-7 w-[400px] bg-gradient-to-r from-[#78dbfe] via-[#e387fb] to-transparent"></div>
+          </div>
+          <h2 className="text-lg text-muted-foreground lg:text-2xl">
             {heroHeader.subheader}
           </h2>
         </div>
-        <Link
-          href="https://github.com/redpangilinan/next-shadcn-landing"
-          target="_blank"
-          className={`w-[10rem] ${cn(buttonVariants({ size: "lg" }))}`}
-        >
-          Get started
-        </Link>
-      </div>
-      {heroHeader.image !== "" ? (
-        <div className="flex flex-1 justify-center lg:justify-end">
-          <Image
-            src={heroHeader.image}
-            width={500}
-            height={500}
-            alt="Header image"
-          />
+        <div className="flex gap-2">
+          <Link
+            href="https://github.com/redpangilinan/next-shadcn-landing"
+            target="_blank"
+            className={`w-[10rem] ${cn(buttonVariants({ size: "lg" }))}`}
+          >
+            Туршиж үзэх
+          </Link>
+          <Link
+            href="https://github.com/redpangilinan/next-shadcn-landing"
+            target="_blank"
+            className={`w-[10rem] ${cn(
+              buttonVariants({ size: "lg", variant: "outline" }),
+            )}`}
+          >
+            Криптограф
+          </Link>
         </div>
-      ) : (
-        <></>
-      )}
+      </div>
+      <div className="animate-randomBounce relative mt-0  flex-1">
+        <Image
+          src="/cloud.png"
+          width={1024}
+          height={1024}
+          alt="Cloud sign hero image"
+          className="w-full"
+          priority
+        />
+        {/* <div className="absolute left-1/2 top-1/2 w-96 -translate-x-1/2 -translate-y-1/2 transform animate-pulse bg-red-200">
+          hehe
+        </div> */}
+      </div>
     </section>
   );
 }
